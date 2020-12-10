f24eaf91dcba418dba0213d54983bf859356ee32
BalashovM <mi-h-a@mail.ru>
2020-12-10 15:08:56 +0700

    Lesson01 - Homework

---
+++ Lessons/Lesson 01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Представьтесь пожалуйста, как вас зовут: ");
+            String UserName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Привет, {UserName}, сегодня {DateTime.Today.ToString("d")} текущее время {(DateTime.Now).ToString("HH:mm:ss")}!");
+            Console.ReadKey(); 
+        }
+    }
+}
