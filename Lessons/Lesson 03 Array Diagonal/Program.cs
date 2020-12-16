03c777e94cbc8238278c592a0c16f807f8a439e5
BalashovM <mi-h-a@mail.ru>
2020-12-17 01:21:33 +0700

    Домашнее задание урок 3 начало

---
+++ Lessons/Lesson 03 Array Diagonal/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_03_Array_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветсвует программа вывода заначений диагонали двумерного массива.");
+        }
+    }
+}
---
+++ Lessons/Lesson 03 Backwards/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_03_Backwards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветсвует программа вывода строки задом наперёд.");
+        }
+    }
+}
